b36243f81dd69d4d7f98bd9f8f652398eb5b47b8
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-02-19 00:05:01 +0100

    better do it urself..

--- front/src/components/Paintings.js
+++ front/src/components/Paintings.js
@@ -1,13 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import { paintingData } from '../data/paintingData';
+import Masonry from 'react-masonry-css';
 import '../styles/paintings.css';
 
 function Painting() {
   const navigate = useNavigate();
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
   return (
     <>
       <section className="section--list">
         <h2>Gallerie</h2>
+
         <div className="paintings--list">
           {paintingData.map((painting) => {
             return (
@@ -17,14 +25,19 @@ function Painting() {
                   navigate(`${painting.id}`);
                 }}
               >
-                {console.log(painting)}
-                <h1>{painting.name}</h1>
-                {/* <p>{painting.description}</p> */}
-                <img
-                  className="painting--list-img"
-                  alt="test"
-                  src={require(`../assets/mini/${painting.cover}`)}
-                />
+                <Masonry
+                  breakpointCols={breakpointColumnsObj}
+                  className="my-masonry-grid"
+                  columnClassName="my-masonry-grid_column"
+                >
+                  <h1>{painting.name}</h1>
+                  <p>{painting.description}</p>
+                  <img
+                    className="painting--list-img"
+                    alt="test"
+                    src={require(`../assets/mini/${painting.cover}`)}
+                  />
+                </Masonry>
               </div>
             );
           })}
