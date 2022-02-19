cc1e2061f1aeab44a1ef4a451278a074e442f6a7
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-02-19 22:35:48 +0100

    carousel working

--- front/src/components/PaintingDisplay.js
+++ front/src/components/PaintingDisplay.js
@@ -1,19 +1,42 @@
 import { Outlet, useParams } from 'react-router-dom';
 import { paintingData } from '../data/paintingData';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import '../styles/painting.css';
 
 function PaintingDisplay() {
   const { id } = useParams();
+  const photos = paintingData[id - 1].photo;
+  photos.map((photo) => console.log(photo));
+
+  console.log(photos);
   return (
     <div className="listOfPaintings">
-      <div className="paintingDisplay">
-        <h1>{paintingData[id - 1].name}</h1>{' '}
+      <section className="paintingDisplay">
+        <h2>{paintingData[id - 1].name}</h2>{' '}
         <p>{paintingData[id - 1].description}</p>{' '}
-        <img
+        {/* <img
           className="painting--display-img"
           alt={paintingData[id - 1].name}
-          src={require(`../assets/mini/${paintingData[id - 1].cover}`)}
-        />
-      </div>
+          src={require(`../assets/mini/${photos}`)}
+        /> */}
+        <div className="carousel-wrapper">
+          <Carousel>
+            {photos.map((photo) => {
+              return (
+                <div key={photo} className="image-wrapper">
+                  <img
+                    className="painting--one-img"
+                    alt={photo}
+                    src={require(`../assets/big/${photo}`)}
+                  />
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
+      </section>
     </div>
   );
 }
